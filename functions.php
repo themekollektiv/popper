09934dd3b5866d3d6f2b50173210f89943173f50
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-03-15 11:35:54 -0700

    Add theme support for custom-logo

--- functions.php
+++ functions.php
@@ -36,6 +36,15 @@ function popper_setup() {
 	add_theme_support( 'title-tag' );
 
 	/*
+	 * Add new image sizes and theme support for Custom Logo
+	 */
+	add_image_size( 'popper-logo', 270, 270 );
+	add_theme_support( 'custom-logo', array(
+   		'size' => 'popper-logo',
+		)
+	);
+
+	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
