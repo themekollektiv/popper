41c72fa897367a47397b6d0b0d7cf814d2b6d8e8
Frank Bültge <frank@bueltge.de>
2016-12-05 16:29:25 +0100

    Remove unnecessary php closing tags

--- single.php
+++ single.php
@@ -12,18 +12,15 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<?php while ( have_posts() ) : the_post();
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+			get_template_part( 'template-parts/content', 'single' );
 
-			<?php
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
 			endif;
-			?>
 
-			<?php
 			// Previous/next post navigation.
 			the_post_navigation( array(
 				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'popper' ) . '</span> ' .
@@ -33,12 +30,13 @@ get_header(); ?>
 				               '<span class="screen-reader-text">' . __( 'Previous post:', 'popper' ) . '</span> ' .
 				               '<span class="post-title">%title</span>',
 			) );
-			?>
 
-		<?php endwhile; // End of the loop. ?>
+		endwhile; // End of the loop. ?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
+?>
