fa3ddd1344e3eaa5c6363448d0303c260c4b8003
Frank Bültge <frank@bueltge.de>
2016-12-05 16:17:27 +0100

    Format for WP Code style rules.

--- index.php
+++ index.php
@@ -7,15 +7,15 @@
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * @link    https://codex.wordpress.org/Template_Hierarchy
  *
  * @package popper
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
@@ -29,18 +29,18 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
-					// Is this the first post of the front page?
-					$first_post = $wp_query->current_post == 0 && !is_paged() && is_front_page();
-					/*
-					 * Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					if ( $first_post == true ) {
-						get_template_part( 'template-parts/content', 'single' );
-					} else {
-						get_template_part( 'template-parts/content', get_post_format() );
-					}
+				// Is this the first post of the front page?
+				$first_post = $wp_query->current_post == 0 && ! is_paged() && is_front_page();
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				if ( $first_post == TRUE ) {
+					get_template_part( 'template-parts/content', 'single' );
+				} else {
+					get_template_part( 'template-parts/content', get_post_format() );
+				}
 
 				?>
 
@@ -54,8 +54,8 @@ get_header(); ?>
 
 		<?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
