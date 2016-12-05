e390b740ea417237fd8406713518b79813da98c2
Frank Bültge <frank@bueltge.de>
2016-12-05 16:20:22 +0100

    Set strict type and reference to qp_query class

--- index.php
+++ index.php
@@ -29,14 +29,18 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
-				// Is this the first post of the front page?
-				$first_post = $wp_query->current_post == 0 && ! is_paged() && is_front_page();
+				/**
+				 * Is this the first post of the front page?
+				 *
+				 * @var \WP_Query $wp_query
+				 */
+				$first_post = (boolean) $wp_query->current_post === 0 && ! is_paged() && is_front_page();
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				if ( $first_post == TRUE ) {
+				if ( $first_post ) {
 					get_template_part( 'template-parts/content', 'single' );
 				} else {
 					get_template_part( 'template-parts/content', get_post_format() );
