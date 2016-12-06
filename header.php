00a8ef00200839b67dd6655a0ac3d08660c0783a
Frank Bültge <frank@bueltge.de>
2016-12-06 13:27:51 +0100

    Formatting, WP Codex.

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
  *
  * This is the template that displays all of the <head> section and everything up until <div id="content">
  *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * @link    https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package popper
  */
@@ -12,12 +12,12 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
@@ -25,51 +25,61 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'popper' ); ?></a>
 
 	<?php
-		if ( get_header_image() ) { ?>
-			<header id="masthead" class="site-header header-background-image" style="background-image: url(<?php echo get_header_image(); ?>) " role="banner">
+	if ( get_header_image() ) { ?>
+	<header id="masthead" class="site-header header-background-image" style="background-image: url(<?php echo get_header_image(); ?>) " role="banner">
 		<?php } else { ?>
-			<header id="masthead" class="site-header" role="banner">
-		<?php }
-		?>
+		<header id="masthead" class="site-header" role="banner">
+			<?php }
+			?>
 
-		<div class="site-logo">
-			<?php $site_title = get_bloginfo( 'name' ); ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-				<div class="screen-reader-text">
-					<?php printf( esc_html__('Go to the home page of %1$s', 'popper'), $site_title ); ?>
-				</div>
-				<?php
+			<div class="site-logo">
+				<?php $site_title = get_bloginfo( 'name' ); ?>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+					<div class="screen-reader-text">
+						<?php printf( esc_html__( 'Go to the home page of %1$s', 'popper' ), $site_title ); ?>
+					</div>
+					<?php
 					// Display logo if Custom Logo or Site Icon is defined, otherwise display First Letter
 					if ( popper_custom_logo() ) {
 						echo popper_custom_logo();
 					} else { ?>
 						<div class="site-firstletter" aria-hidden="true">
-							<?php echo substr($site_title, 0, 1); ?>
+							<?php echo substr( $site_title, 0, 1 ); ?>
 						</div>
-				<?php } ?>
-			</a>
-		</div>
+					<?php } ?>
+				</a>
+			</div>
 
-		<div class="site-branding<?php if ( !is_front_page() && is_singular() ) { echo ' screen-reader-text'; } ?>">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<div class="site-branding<?php if ( ! is_front_page() && is_singular() ) {
+				echo ' screen-reader-text';
+			} ?>">
+				<?php if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</h1>
+				<?php else : ?>
+					<p class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</p>
+				<?php endif; ?>
+				<?php
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php endif; ?>
+			</div><!-- .site-branding -->
+
+
+			<?php if ( has_nav_menu( 'primary' ) ) : ?>
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'popper' ); ?></button>
+					<?php wp_nav_menu( array(
+						'theme_location' => 'primary',
+						'menu_id'        => 'primary-menu',
+						'menu_class'     => 'nav-menu'
+					) ); ?>
+				</nav><!-- #site-navigation -->
 			<?php endif; ?>
-			<?php
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		</header><!-- #masthead -->
 
-
-		<?php if ( has_nav_menu( 'primary' ) ) : ?>
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'popper' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'menu_class' => 'nav-menu' ) ); ?>
-			</nav><!-- #site-navigation -->
-		<?php endif; ?>
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
