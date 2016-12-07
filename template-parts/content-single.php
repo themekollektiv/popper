cd131779d97065e93e33fc5ab3e24896e8cce8eb
Frank Bültge <frank@bueltge.de>
2016-12-07 11:26:02 +0100

    Formatting, WP Codex

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -25,7 +25,13 @@
 		<?php }
 
 		if ( popper_is_first_post() ) {
-			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			the_title(
+				sprintf(
+					'<h2 class="entry-title"><a href="%s" rel="bookmark">',
+					esc_url( get_permalink() )
+				),
+				'</a></h2>'
+			);
 		} else {
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		}
