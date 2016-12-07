ce4e17fefe01d3f2706ede5a59e5fa36b22ba391
Frank Bültge <frank@bueltge.de>
2016-12-07 12:41:28 +0100

    Formatting, WP Codex

--- inc/customizer.php
+++ inc/customizer.php
@@ -11,20 +11,21 @@
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function popper_customize_register( $wp_customize ) {
+
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 
 	/**
-	* Custom Customizer Customizations
-	*/
+	 * Custom Customizer Customizations
+	 */
 
 	// Create heading color setting
-	$wp_customize->add_setting( 'header_color' , array(
-		'default'     => '#000000',
-		'type' => 'theme_mod',
-		'sanitize_callback'    => 'sanitize_hex_color',
-		'transport'   => 'postMessage',
+	$wp_customize->add_setting( 'header_color', array(
+		'default'           => '#000000',
+		'type'              => 'theme_mod',
+		'sanitize_callback' => 'sanitize_hex_color',
+		'transport'         => 'postMessage',
 	) );
 
 	// Add the controls
@@ -32,8 +33,8 @@ function popper_customize_register( $wp_customize ) {
 		new WP_Customize_Color_Control(
 			$wp_customize,
 			'link_color', array(
-				'label' => __( 'Header Background Color', 'popper' ),
-				'section' => 'colors',
+				'label'    => __( 'Header Background Color', 'popper' ),
+				'section'  => 'colors',
 				'settings' => 'header_color'
 			)
 		)
@@ -43,49 +44,50 @@ function popper_customize_register( $wp_customize ) {
 	// Add option to select sidebar position
 	$wp_customize->add_section( 'popper_options',
 		array(
-			'title' => __( 'Theme Options', 'popper' ),
-//			'priority' => 95,
-			'capability' => 'edit_theme_options',
+			'title'       => __( 'Theme Options', 'popper' ),
+			//			'priority' => 95,
+			'capability'  => 'edit_theme_options',
 			'description' => __( 'Change the default display options for the theme.', 'popper' )
 		)
 	);
 
 	// Create settings
-	$wp_customize->add_setting(	'layout_setting',
+	$wp_customize->add_setting( 'layout_setting',
 		array(
-			'default' => 'no-sidebar',
-			'type' => 'theme_mod',
+			'default'           => 'no-sidebar',
+			'type'              => 'theme_mod',
 			'sanitize_callback' => 'popper_sanitize_layout', // Sanitization function appears further down
-			'transport' => 'postMessage'
+			'transport'         => 'postMessage'
 		)
 	);
 
 	// Add the controls
-	$wp_customize->add_control(	'popper_layout_control',
+	$wp_customize->add_control( 'popper_layout_control',
 		array(
-			'type' => 'radio',
-			'label' => __( 'Sidebar position', 'popper' ),
-			'section' => 'popper_options',
-			'choices' => array(
-				'no-sidebar' => __( 'No sidebar (default)', 'popper' ),
-				'sidebar-left' => __( 'Left sidebar', 'popper' ),
+			'type'     => 'radio',
+			'label'    => __( 'Sidebar position', 'popper' ),
+			'section'  => 'popper_options',
+			'choices'  => array(
+				'no-sidebar'    => __( 'No sidebar (default)', 'popper' ),
+				'sidebar-left'  => __( 'Left sidebar', 'popper' ),
 				'sidebar-right' => __( 'Right sidebar', 'popper' )
 			),
 			'settings' => 'layout_setting' // Matches setting ID from above
 		)
 	);
 }
+
 add_action( 'customize_register', 'popper_customize_register' );
 
 /**
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function popper_customize_preview_js() {
+
 	wp_enqueue_script( 'popper_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
+
 add_action( 'customize_preview_init', 'popper_customize_preview_js' );
-
-
 
 
 /**
@@ -95,10 +97,12 @@ add_action( 'customize_preview_init', 'popper_customize_preview_js' );
  */
 
 function popper_sanitize_layout( $value ) {
-    if ( ! in_array( $value, array( 'sidebar-left', 'sidebar-right', 'no-sidebar' ) ) )
-        $value = 'no-sidebar';
 
-    return $value;
+	if ( ! in_array( $value, array( 'sidebar-left', 'sidebar-right', 'no-sidebar' ) ) ) {
+		$value = 'no-sidebar';
+	}
+
+	return $value;
 }
 
 /**
@@ -107,20 +111,20 @@ function popper_sanitize_layout( $value ) {
 
 function popper_customize_css() {
 
-	$header_color = get_theme_mod('header_color');
+	$header_color = get_theme_mod( 'header_color' );
 
 	if ( $header_color && $header_color != "#000000" ) {
-	?>
-         <style type="text/css">
-             .site-header {
-				 background-color:<?php echo $header_color; ?>;
-			 }
-         </style>
-    <?php
+		?>
+		<style type="text/css">
+			.site-header {
+				background-color: <?php echo $header_color; ?>;
+			}
+		</style>
+		<?php
 	}
 
-	if ( $header_color == '#ffffff' && !get_header_image() ) {
-	?>
+	if ( $header_color == '#ffffff' && ! get_header_image() ) {
+		?>
 		<style type="text/css">
 			.main-navigation {
 				background-color: transparent;
@@ -161,7 +165,8 @@ function popper_customize_css() {
 				}
 			}
 		</style>
-	<?php
+		<?php
 	}
 }
-add_action( 'wp_head', 'popper_customize_css');
+
+add_action( 'wp_head', 'popper_customize_css' );
