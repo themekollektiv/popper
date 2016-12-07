5817096529df5ebeb27a5398f4f13c8c16272473
Frank Bültge <frank@bueltge.de>
2016-12-07 15:27:54 +0100

    Fix different topics for strict coding

--- widgets/recent-comments.php
+++ widgets/recent-comments.php
@@ -5,21 +5,26 @@
  * Gently lifted and reworked from Anders Norén's Lovecraft theme: http://www.andersnoren.se/teman/lovecraft-wordpress-theme/
  */
 
+register_widget( 'popper_recent_comments' );
+
 class popper_recent_comments extends WP_Widget {
 
 	public function __construct() {
 
 		$widget_ops = array(
 			'classname'   => 'widget_popper_recent_comments',
-			'description' => __( 'Displays recent comments with user avatar and excerpt.', 'popper' )
+			'description' => esc_html__( 'Displays recent comments with user avatar and excerpt.', 'popper' )
 		);
-		parent::__construct( 'widget_popper_recent_comments', __( 'Enhanced Recent Comments', 'popper' ), $widget_ops );
+		parent::__construct(
+			'widget_popper_recent_comments', esc_html__( 'Enhanced Recent Comments', 'popper' ),
+			$widget_ops
+		);
 	}
 
 	public function widget( $args, $instance ) {
 
 		// Outputs the content of the widget
-		extract( $args ); // Make before_widget, etc available.
+		extract( $args, EXTR_SKIP ); // Make before_widget, etc available.
 
 		$widget_title       = NULL;
 		$number_of_comments = NULL;
@@ -27,7 +32,9 @@ class popper_recent_comments extends WP_Widget {
 		$widget_title       = esc_attr( apply_filters( 'widget_title', $instance[ 'widget_title' ] ) );
 		$number_of_comments = esc_attr( $instance[ 'number_of_comments' ] );
 
-		echo $before_widget;
+		echo isset( $before_widget ) ? $before_widget : '';
+		$before_title = isset( $before_title ) ? $before_title : '';
+		$after_title  = isset( $after_title ) ? $after_title : '';
 
 		if ( ! empty( $widget_title ) ) {
 			echo $before_title . $widget_title . $after_title;
@@ -36,11 +43,10 @@ class popper_recent_comments extends WP_Widget {
 		}
 		?>
 
-		<ul class="popper-widget-list">
+		<ul class="popper-widget-list" xmlns="http://www.w3.org/1999/html">
 
 			<?php
-
-			if ( $number_of_comments == 0 ) {
+			if ( 0 === $number_of_comments ) {
 				$number_of_comments = 5;
 			}
 
@@ -58,7 +64,7 @@ class popper_recent_comments extends WP_Widget {
 
 			// Comment Loop
 			if ( $comments ) {
-				foreach ( $comments as $comment ) { ?>
+				foreach ( (array) $comments as $comment ) { ?>
 
 					<li>
 						<a href="<?php echo get_permalink( $comment->comment_post_ID ); ?>#comment-<?php echo $comment->comment_ID; ?>">
@@ -66,9 +72,9 @@ class popper_recent_comments extends WP_Widget {
 								<?php echo get_avatar( get_comment_author_email( $comment->comment_ID ), $size = '96' ); ?>
 							</div>
 							<p class="title"><span><?php comment_author(); ?></span></p>
-							<p class="excerpt"><?php echo esc_attr( comment_excerpt( $comment->comment_ID ) ); ?></p>
+							<p class="excerpt"><?php echo esc_attr( get_comment_excerpt( $comment->comment_ID ) ); ?></p>
 							<p class="original-title">
-								<span><?php _e( 'on', 'popper' ); ?></span> <?php the_title_attribute( array( 'post' => $comment->comment_post_ID ) ); ?>
+								<span><?php esc_attr_e( 'on', 'popper' ); ?></span> <?php the_title_attribute( array( 'post' => $comment->comment_post_ID ) ); ?>
 							</p>
 						</a>
 					</li>
@@ -79,7 +85,8 @@ class popper_recent_comments extends WP_Widget {
 
 		</ul>
 
-		<?php echo $after_widget;
+		<?php
+		echo isset( $after_widget ) ? $after_widget : '';
 	}
 
 
@@ -89,21 +96,21 @@ class popper_recent_comments extends WP_Widget {
 
 		$instance[ 'widget_title' ] = strip_tags( $new_instance[ 'widget_title' ] );
 		// make sure we are getting a number
-		$instance[ 'number_of_comments' ] = is_int( intval( $new_instance[ 'number_of_comments' ] ) ) ? intval( $new_instance[ 'number_of_comments' ] ) : 5;
+		$instance[ 'number_of_comments' ] = is_int( (int) $new_instance[ 'number_of_comments' ] )
+			? (int) $new_instance[ 'number_of_comments' ] : 5;
 
 		//update and save the widget
 		return $instance;
-
 	}
 
 	public function form( $instance ) {
 
 		// Set defaults
-		if ( ! isset( $instance[ "widget_title" ] ) ) {
-			$instance[ "widget_title" ] = '';
+		if ( ! isset( $instance[ 'widget_title' ] ) ) {
+			$instance[ 'widget_title' ] = '';
 		}
-		if ( ! isset( $instance[ "number_of_comments" ] ) ) {
-			$instance[ "number_of_comments" ] = '5';
+		if ( ! isset( $instance[ 'number_of_comments' ] ) ) {
+			$instance[ 'number_of_comments' ] = '5';
 		}
 
 		// Get the options into variables, escaping html characters on the way
@@ -113,7 +120,8 @@ class popper_recent_comments extends WP_Widget {
 
 		<p>
 			<label for="<?php echo esc_attr( $this->get_field_id( 'widget_title' ) ); ?>"><?php _e( 'Title', 'popper' ); ?>:
-				<input id="<?php echo esc_attr( $this->get_field_id( 'widget_title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'widget_title' ) ); ?>" type="text" class="widefat" value="<?php echo esc_attr( $widget_title ); ?>" /></label>
+				<input id="<?php echo esc_attr( $this->get_field_id( 'widget_title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'widget_title' ) ); ?>" type="text" class="widefat" value="<?php echo esc_attr( $widget_title ); ?>" />
+			</label>
 		</p>
 
 		<p>
@@ -126,12 +134,11 @@ class popper_recent_comments extends WP_Widget {
 	}
 }
 
-register_widget( 'popper_recent_comments' );
-
 /**
  * Remove the line above and uncomment the lines below to replace the default widget
+ *
+ * @todo We should remove this, is more a task for a plugin, no relevance for a theme.
  */
-
 /*
 function popper_comments_widget_registration() {
 	unregister_widget('WP_Widget_Recent_Comments');
