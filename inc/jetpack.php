b187a588c1dfdc2b4619963e0b03c73e25b89c7b
Frank Bültge <frank@bueltge.de>
2016-12-07 12:40:09 +0100

    Formatting, WP Codex

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -2,7 +2,7 @@
 /**
  * Jetpack Compatibility File.
  *
- * @link https://jetpack.me/
+ * @link    https://jetpack.me/
  *
  * @package popper
  */
@@ -12,6 +12,7 @@
  * See: https://jetpack.me/support/infinite-scroll/
  */
 function popper_jetpack_setup() {
+
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
 		'render'    => 'popper_infinite_scroll_render',
@@ -24,6 +25,7 @@ add_action( 'after_setup_theme', 'popper_jetpack_setup' );
  * Custom render function for Infinite Scroll.
  */
 function popper_infinite_scroll_render() {
+
 	while ( have_posts() ) {
 		the_post();
 		get_template_part( 'template-parts/content', get_post_format() );
