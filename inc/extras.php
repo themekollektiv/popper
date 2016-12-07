cf37bfc9e56e22561e0b09f62ffbbb0a3bd0cb26
Frank Bültge <frank@bueltge.de>
2016-12-07 12:41:00 +0100

    Formatting, WP Codex

--- inc/extras.php
+++ inc/extras.php
@@ -11,9 +11,11 @@
  * Adds custom classes to the array of body classes.
  *
  * @param array $classes Classes for the body element.
+ *
  * @return array
  */
 function popper_body_classes( $classes ) {
+
 	// Adds a class of group-blog to blogs with more than 1 published author.
 	if ( is_multi_author() ) {
 		$classes[] = 'group-blog';
@@ -21,4 +23,5 @@ function popper_body_classes( $classes ) {
 
 	return $classes;
 }
+
 add_filter( 'body_class', 'popper_body_classes' );
